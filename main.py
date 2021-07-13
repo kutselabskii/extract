23e7d2967ca61fcd19a9873061cb5254f158a491
Egor Kutselabskii <osutanfirst@gmail.com>
2021-07-13 20:40:00 +1000

    Failes to compile due to deprecation of ListView feature

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.utils import platform
+
+import os
+
+class FolderSelector(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(FolderSelector, self).__init__(**kwargs)
+        self.drives_list.adapter.bind(on_selection_change=self.drive_selection_changed)
+
+    def is_dir(self, directory, filename):
+        return os.path.isdir(os.path.join(directory, filename))
+
+    def get_win_drives(self):
+            if platform == 'win':
+                import win32api
+
+                drives = win32api.GetLogicalDriveStrings()
+                drives = drives.split('\000')[:-1]
+
+                return drives
+            else:
+                return []
+
+    def drive_selection_changed(self, *args):
+        self.file_chooser.path = args[0].selection[0].text
 
 class MainScreen(Widget):
     def parse(self):
@@ -8,6 +39,22 @@ class MainScreen(Widget):
         filenames = list(map(lambda x: self.ids.template.text.replace('*', x), numbers))
 
         self.ids.parsed.text = '\n'.join(filenames)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_source_selector(self):
+        content = FolderSelector(load=self.select_source, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Select source folder", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def select_source(self, path, filename):
+        print(path, filename)
+
+        self.dismiss_popup()
+
+    def select_target(self, path, filename):
+        pass
 
 class ExtractApp(App):
     def build(self):
